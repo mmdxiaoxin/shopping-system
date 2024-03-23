2adabcca6be651780eb4071a37ba74997b9b4659
小新 <782446723@qq.com>
2024-03-23 20:28:57 +0800

    feat: 封装工具类

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import {useCookies} from '@vueuse/integrations/useCookies';
-import {ElNotification} from "element-plus";
+import {getToken} from "@/utils/auth";
+import {toast} from "@/utils/common";
 
 const config = {
     // 默认地址请求地址，可在 .env.** 文件中修改
@@ -13,12 +13,12 @@ const config = {
 
 const service = axios.create(config);
 
+// 请求拦截器
 service.interceptors.request.use((config) => {
     // 在请求头中添加 token
-    const cookie = useCookies()
-    const token = cookie.get("admin-token")
+    const token = getToken()
     if (token) {
-        config.headers["token"] = token
+        config.headers['token'] = token;
     }
 
     return config;
@@ -27,15 +27,14 @@ service.interceptors.request.use((config) => {
     return Promise.reject(error);
 });
 
+//响应拦截器
 service.interceptors.response.use((response) => {
+
+    //处理响应数据
     return response.data.data;
 }, (error) => {
 
-    ElNotification({
-        message: error.response.data.msg || "请求失败",
-        type: 'error',
-        duration: 3000
-    })
+    toast(error.response.data.msg || "请求失败","error")
 
     return Promise.reject(error);
 });
