d22e13730f7e6a5c1798a86c4d33c0c9bd5b76fe
小新 <782446723@qq.com>
2024-04-29 18:33:47 +0800

    chore: postcss更新

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import {createApp} from 'vue'
-import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import "element-plus/dist/index.css";
+import App from "./App.vue";
 
-
-import './styles/main.css'
+import "./styles/main.css";
 import router from "@/router";
 import store from "@/stores";
 import "nprogress/nprogress.css";
@@ -25,4 +24,4 @@ app.use(store);
 // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 //     app.component(key, component)
 // }
-app.mount('#app');
+app.mount("#app");
