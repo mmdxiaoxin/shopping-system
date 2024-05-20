a5316ed4b7843f6c35208932046897f8a866ad1d
小新 <782446723@qq.com>
2024-05-20 16:48:26 +0800

    feat: 防抖节流指令封装

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,7 +1,11 @@
 import auth from "./modules/auth";
+import debounce from "@/directives/modules/debounce";
+import throttle from "@/directives/modules/throttle";
 
 const directivesList = {
   auth,
+  debounce,
+  throttle,
 };
 
 const directives = {
---
+++ src/directives/modules/debounce.js
@@ -0,0 +1,22 @@
+const debounce = {
+  mounted(el, binding) {
+    if (typeof binding.value !== "function") {
+      throw "callback must be a function";
+    }
+    let timer = null;
+    el.__handleClick__ = function () {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        binding.value();
+      }, 500);
+    };
+    el.addEventListener("click", el.__handleClick__);
+  },
+  beforeUnmount(el) {
+    el.removeEventListener("click", el.__handleClick__);
+  },
+};
+
+export default debounce;
