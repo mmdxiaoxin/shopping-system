3e58467b8c35522956fe8546ed1202adc648d861
小新 <782446723@qq.com>
2024-06-09 16:23:52 +0800

    refactor: 默认渲染返回的HTML元素

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,35 +1,39 @@
-import {ElMessageBox, ElNotification} from 'element-plus'
-import nprogress from 'nprogress'
+import { ElMessageBox, ElNotification } from "element-plus";
+import nprogress from "nprogress";
 
 // 消息提示
-export const toast = (message, type = "success", dangerouslyUseHTMLString = false) => {
-    ElNotification({
-        message,
-        type,
-        dangerouslyUseHTMLString,
-        duration: 3000
-    })
-}
+export const toast = (
+  message,
+  type = "success",
+  dangerouslyUseHTMLString = true,
+) => {
+  ElNotification({
+    message,
+    type,
+    dangerouslyUseHTMLString,
+    duration: 3000,
+  });
+};
 
 // 显示全局Loading
 export const showFullLoading = () => {
-    nprogress.start()
-}
+  nprogress.start();
+};
 
 // 隐藏全局Loading
 export const hideFullLoading = () => {
-    nprogress.done()
-}
+  nprogress.done();
+};
 
 // 模态框确认提示
-export const showModal = (content = "提示内容", type = "warning", title = "") => {
-    return ElMessageBox.confirm(
-        content,
-        title,
-        {
-            confirmButtonText: '确认',
-            cancelButtonText: '取消',
-            type,
-        }
-    )
-}
+export const showModal = (
+  content = "提示内容",
+  type = "warning",
+  title = "",
+) => {
+  return ElMessageBox.confirm(content, title, {
+    confirmButtonText: "确认",
+    cancelButtonText: "取消",
+    type,
+  });
+};
